7eafde058b9cea22bcd4eb26d04787ce04448357
MBEDOBE <danielbushbaby@gmail.com>
2022-02-08 01:47:35 +0000

    moved logo to assets folder

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -8,7 +8,7 @@ function Header() {
   return (
     <div className="header">
       <div className="logo">
-        <img src="/logo.png" alt="logo" />
+        <img src="../../assets/logo.png" alt="logo" />
       </div>
       <div className="menu">
         <div className="web-menu">
